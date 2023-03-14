1c67e64a72b0706f0f531288a7ae3c198a9bf639
jaya vignesh <rajr095025@gmail.com>
2023-03-14 16:53:36 +0530

    changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -8,23 +8,46 @@ public class Pizza {
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg == true){
+            this.price = 300;
+        }
+        else{
+            this.price = 400;
+        }
+        this.bill = Integer.toString(this.price);
         // your code goes here
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public void addExtraCheese(){
-        // your code goes here
+        price += 80;
+        this.bill = Integer.toString(price);
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.isVeg == true){
+            price += 70;
+            this.bill = Integer.toString(price);
+        }
+        else{
+            price += 120;
+            this.bill = Integer.toString(price);
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        // your cod
+        price += 20;
+        this.bill = Integer.toString(price);
+
     }
 
     public String getBill(){
